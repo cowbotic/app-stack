345ef7a50ac28fadf968479b20a22a059f3541dd
cowbotic <mario.arienza@capside.com>
2018-12-17 16:25:59 +0100

    Nuevo script de inicio

--- config_files/start_flask_app.sh
+++ config_files/start_flask_app.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+echo "--Start Script"
 #If there is a debug file, just run the app in debug mode
 if [ -e /debug ]; then
         echo "App running in debug mode"
         python3 app.py
 #Otherwise, install OS packages and python libraries and start in standard mode
 else
-  if [ -e os-packets.txt ]; then
-    apk add --no-cache $(cat os-packets.txt)
+  if [ -e /flask-app/os-packets.txt ]; then
+    echo "Installing OS packages"
+    apk add --no-cache $(cat /flask-app/os-packets.txt) > /dev/null
   fi
-  if [ -e requirements.txt ]; then
-    pip3 install -r python_libraries.txt
+  if [ -e /flask-app/python_libraries.txt ]; then
+    echo "Installing Python packages"
+    pip3 install -r /flask-app/python_libraries.txt > /dev/null
   fi
   echo "App running in production mode"
-  nginx && uwsgi --ini /flask-app.ini
+  nginx && uwsgi --ini /flask_app.ini
 fi
+echo "--End Start Script"
