58aa1aeb3f5d1afa31334c184667cb3efd3d2eaf
cowbotic <mario.arienza@capside.com>
2019-01-18 12:40:32 +0100

    build automatico

--- app.py
+++ app.py
@@ -17,47 +17,66 @@ from opencensus.trace.ext.flask.flask_middleware import FlaskMiddleware
 
 #exporter = print_exporter.PrintExporter()
 #tracer = Tracer(sampler=always_on.AlwaysOnSampler(), exporter=exporter)
-
-zipkin_server='zipkin-server'
-ze = ZipkinExporter(service_name="flask-app",
-                                host_name=zipkin_server,
-                                port=9411,
-                                endpoint='/api/v2/spans')
-
-tracer = Tracer(exporter=ze, sampler=always_on.AlwaysOnSampler())
-
-def traza_main(dict_attr,nombre='ElPadre'):
-  with tracer.span(name='ElPadre') as span:
-    traza(dict_attr) 
-
-def traza(dict_attr={}):
-  with tracer.span() as span: 
-    for elem in dict_attr:
-      tracer.add_attribute_to_current_span(attribute_key=elem, attribute_value=dict_attr[elem])
-
-
-#Cabeceras http modificadas para algunas respuestas en JSON...
 headers={'mimetype':'application/json','Content-Type':'application/json','Server':'Who cares'}
 redis_server = 'redis-server'
+zipkin_server='zipkin-server'
+
+#Conectamos a ver si hay zipkin
+try_trazas=False
+TRAZAS=False
+if try_trazas:
+  try:
+    zipkin_server='zipkin-server'
+    ze = ZipkinExporter(service_name="flask-app",host_name=zipkin_server,port=9411,endpoint='/api/v2/spans')
+    tracer = Tracer(exporter=ze, sampler=always_on.AlwaysOnSampler())
+    TRAZAS=True
+    def traza_main(dict_attr,nombre='ElPadre'):
+      with tracer.span(name='ElPadre') as span:
+        traza(dict_attr) 
+
+    def traza(dict_attr={}):
+      with tracer.span() as span: 
+        for elem in dict_attr:
+          tracer.add_attribute_to_current_span(attribute_key=elem, attribute_value=dict_attr[elem])
+
+  except Exception:
+    print({'Trazas':'No hay backend de trazas'})
+    params_trazas={"trazas":"NOT running"}
+else:
+  print({'Trazas':'No se ponen'})
+  params_trazas={"trazas":"NOT Configured"}
+
 
 #Conectamos a ver si hay Redis...
-try:
-  dstore = redis.Redis(redis_server)
-  dstore.ping()
-  dstore.incr('conexiones',1)
-  REDIS_UP=True
-  params_redis={"redis":"ready"}
-except redis.ConnectionError:
-  params_redis={"redis":"NOT ready"}
-  print('{"redis":"Redis no esta disponible"}')
-  REDIS_UP=False
-traza_main(params_redis,'RedisConn')
+try_redis=True
+REDIS_UP=False
+if try_redis:
+  try:
+    dstore = redis.Redis(redis_server,socket_timeout=2,socket_connect_timeout=3)
+    dstore.ping()
+    dstore.incr('conexiones',1)
+    REDIS_UP=True
+    params_redis={"redis":"ready"}
+  except Exception:
+    print('{"redis":"Redis no esta disponible"}')
+    params_redis={"redis":"NOT running"}
+  if TRAZAS:
+    traza_main(params_redis,'RedisConn')
+else:
+  print('{"redis":"Redis no se intenta"}')
+  params_redis={"redis":"NOT configured"}
 
-#Funcion para adivinar la direccion IP del servidor...
+#Funciones para adivinar la direccion IP del servidor y metadatos sobre la IP
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     return s.getsockname()[0]
+
+def get_ip_address_data(ip):
+  try:
+    return requests.get('http://ip-api.com/json/'+ip, timeout=(3,5)).json()
+  except Exception:
+    return {'country':'Desconocido','regionName':'Desconocido','isp':'Desconocido'}
 
 #Contadores para Prometheus
 global hits
@@ -68,23 +87,17 @@ if REDIS_UP:
   redis_con=dstore.get('conexiones').decode('utf-8')
 else:
   redis_con=0
-traza_main({'conexiones':str(redis_con)},'RedisCheck')
 
 app = Flask(__name__)
-middleware = FlaskMiddleware(app,exporter=ze)
+
+if TRAZAS:
+  traza_main({'conexiones':str(redis_con)},'RedisCheck')
+  middleware = FlaskMiddleware(app,exporter=ze)
+
 
 
 @app.route('/')
 def home():
-  body={'status':200,'todo':'bien','Redis':str(REDIS_UP)}
-  status=body['status']
-  headers={'mimetype':'application/json','Content-Type':'application/json'}
-
-  resp=make_response((jsonify(body), status, headers))
-  return resp
-
-@app.route('/hello')
-def hello():
   global hits
   hits=hits+1
   params={}
@@ -94,9 +107,9 @@ def hello():
    params['ip'] = request.remote_addr
 
   
-  datos_ip=requests.get('http://ip-api.com/json/'+params['ip'])
+  datos_ip=get_ip_address_data(params['ip'])
   
-  if len(set(['country','region','isp']).intersection(datos_ip.json().keys())) == 3:
+  if len(set(['country','region','isp']).intersection(datos_ip.keys())) == 3:
     params['country']=datos_ip.json()['country']
     params['region']=datos_ip.json()['regionName']
     params['isp']=datos_ip.json()['isp']
@@ -112,59 +125,7 @@ def hello():
 
   return render_template('hello.html', params=params)
 
-@app.route('/fake')
-def fake():
-  global hits
-  hits=hits+1
-  
-  params={}
-  params['ip']          = request.remote_addr
-  params['country' ]    = 'loccitan'
-  params['region']      = 'zenda'
-  params['isp']         = 'Conde Olaf Telecom'
-  params['cont_ip']     = '10.1.2.3'
-  params['pub_cont_ip'] = '1.2.3.4'
-  params['hostname']    = socket.gethostname()
-  params['hora']        = str(datetime.now())
-  params['random']      = str(random.randrange(1,200))
-  params['hora_requ']   = str(request.date)
-  params['method']      = request.method
-  
-  traza_main(params,"/fake")  
-  return render_template('hello.html', params=params)
-
-
-@app.route('/healthz')
-def healthz():
-  body={'status':208,'todo':'bien'}
-  status=body['status']
-  headers={'mimetype':'application/json','Content-Type':'application/json'}
-
-  resp=make_response((jsonify(body), status, headers))
-  return resp
-
-@app.route('/redis')
-def redis():
-  if REDIS_UP:
-    #dstore.incr('conexiones',1)
-    dato=dstore.get('conexiones').decode('utf-8')
-    resp=make_response('{"conexiones":'+str(dato)+',"Servidor":"'+socket.gethostname()+'"}')
-  else:
-    resp=make_response('{"redis":"Error"}')
-    
-  return resp
-
-@app.route('/cabeceras')
-def cabeceras():
-  status=200
-
-  resp_dict={}
-  for elemento in request.headers.keys():
-    resp_dict[str(elemento)]=request.headers.getlist(elemento)[0]
-  
-  resp=make_response((jsonify(resp_dict), status, headers))
-  return resp
-
+#Particulas
 @app.route('/particulas')
 def particulas():
   return render_template('particulas.html')
@@ -203,7 +164,9 @@ def particulasvisitadas():
 
   resp=make_response(jsonify(resp_dict),headers)
   return resp
+#/Particulas
 
+#Elementos
 @app.route('/elementos')
 def elementos():
   return render_template('elementos.html')
@@ -223,7 +186,6 @@ def elementorandom():
 
   return render_template('elementorandom.html', params=props)
 
-
 @app.route('/elementosvisitados')
 def elementosvisitados():
   resp_dict={}
@@ -241,6 +203,89 @@ def elementosvisitados():
     resp_dict={"redis":"Error"}
 
   resp=make_response(jsonify(resp_dict),headers)
+  return resp
+#/Elementos
+
+@app.route('/hello')
+def hello():
+  global hits
+  hits=hits+1
+  params={}
+  if request.headers.getlist("X-Forwarded-For"):
+    params['ip'] = request.headers.getlist("X-Forwarded-For")[0]
+  else:
+   params['ip'] = request.remote_addr
+
+  
+  datos_ip=get_ip_address_data(params['ip'])
+  
+  if len(set(['country','region','isp']).intersection(datos_ip.keys())) == 3:
+    params['country']=datos_ip.json()['country']
+    params['region']=datos_ip.json()['regionName']
+    params['isp']=datos_ip.json()['isp']
+  
+  else:
+    params['country']='desconocido'
+    params['region']='desconocido'
+    params['isp']='desconocido'
+  
+  params['cont_ip']=get_ip_address()
+  params['pub_cont_ip']=requests.get('http://ident.me').text
+  params['hostname']=socket.gethostname()
+
+  return render_template('hello.html', params=params)
+
+@app.route('/fake')
+def fake():
+  global hits
+  hits=hits+1
+  
+  params={}
+  params['ip']          = request.remote_addr
+  params['country' ]    = 'loccitan'
+  params['region']      = 'zenda'
+  params['isp']         = 'Conde Olaf Telecom'
+  params['cont_ip']     = '10.1.2.3'
+  params['pub_cont_ip'] = '1.2.3.4'
+  params['hostname']    = socket.gethostname()
+  params['hora']        = str(datetime.now())
+  params['random']      = str(random.randrange(1,200))
+  params['hora_requ']   = str(request.date)
+  params['method']      = request.method
+  
+  if trazas:
+    traza_main(params,"/fake")  
+  return render_template('hello.html', params=params)
+
+@app.route('/healthz')
+def healthz():
+  body={'status':208,'todo':'bien'}
+  status=body['status']
+  headers={'mimetype':'application/json','Content-Type':'application/json'}
+
+  resp=make_response((jsonify(body), status, headers))
+  return resp
+
+@app.route('/redis')
+def redis():
+  if REDIS_UP:
+    #dstore.incr('conexiones',1)
+    dato=dstore.get('conexiones').decode('utf-8')
+    resp=make_response('{"conexiones":'+str(dato)+',"Servidor":"'+socket.gethostname()+'"}')
+  else:
+    resp=make_response('{"redis":"Error"}')
+    
+  return resp
+
+@app.route('/cabeceras')
+def cabeceras():
+  status=200
+
+  resp_dict={}
+  for elemento in request.headers.keys():
+    resp_dict[str(elemento)]=request.headers.getlist(elemento)[0]
+  
+  resp=make_response((jsonify(resp_dict), status, headers))
   return resp
 
 @app.route('/metrics')
